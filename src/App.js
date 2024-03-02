08a191018265d0bfed43ae5dfab4e35fff7b90e5
HariGadi143 <haridhonigadi0707@gmail.com>
2024-03-02 08:30:15 +0530

    worked on social buttons

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<h1>Social Buttons</h1>
+      <div className="mainContainer">
+        <h1 className="mainHeading">Social Buttons</h1>
+        <div className="buttonsContainer">
+{buttonList.map((item, index) => {
+  return (
+    <button className="buttonItem" key={index} style={{background: `${item?.background}`, color: `${item?.textColor}`}}>{item?.title}</button>
+  )
+})}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+const buttonList=[
+  {
+    id: 1,
+    title: "Like",
+    background:"#eab308",
+    textColor: "white",
+  },
+  {
+    id:2,
+    title: "Comment",
+    background:"white",
+    textColor: " #323f4b",
+  },  {
+    id: 3,
+    title: "Share",
+    background:"#1d4ed8",
+    textColor: "white",
+  },
+]
